0458cc0e187dc373ed435582e7870b51ba2d48fb
Logan Ravinuthala <loganravin4@gmail.com>
2025-01-18 21:47:24 -0500

    got pages to render

--- src/frontend/App.jsx
+++ src/frontend/App.jsx
@@ -1,49 +1,16 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Homepage from "./homepage";
+import UserDashboard from "./UserDashboard";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [message, setMessage] = useState('');
-
-  useEffect(() => {
-    axios.get('http://localhost:5000/api/data')
-      .then(response => {
-        setMessage(response.data.message);
-      })
-      .catch(error => {
-        console.error('There was an error fetching the data!', error);
-      });
-  }, []);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-        <p className="read-the-docs">
-          {message ? `Message from Flask: ${message}` : 'Loading message...'}
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
+        <Route path="/dashboard" element={<UserDashboard />} />
+      </Routes>
+    </div>
   );
 }
 
